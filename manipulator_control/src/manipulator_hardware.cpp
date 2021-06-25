a72a9c36b9f4f263476e2807e3a6fb0692f11409
chaos4ros2 <liuchao4664@yahoo.co.jp>
2021-06-26 07:52:53 +0900

    add argument servo_id_list

--- manipulator_control/src/manipulator_hardware.cpp
+++ manipulator_control/src/manipulator_hardware.cpp
@@ -55,7 +55,7 @@ return_type ManipulatorHardware::configure(const hardware_interface::HardwareInf
     // 青木さんはここでbaudrateをも引数として渡し、open_port()メソッド中でsetBaudRate()を呼び出した。自分もset_feq()を、初期化に追加しよう。
     // open_port()を実装してないから
     // https://github.com/rt-net/crane_plus/blob/use_new_ros2_control/crane_plus_control/src/crane_plus_driver.cpp#L70
-    driver_ = std::make_shared<ManipulatorDriver>(port_name, address, frequency_hz);
+    driver_ = std::make_shared<ManipulatorDriver>(port_name, address, frequency_hz, servo_id_list);
     if (!driver_->open_port()) {
         RCLCPP_ERROR(rclcpp::get_logger("ManipulatorHardware"), driver_->get_last_error_log());
         return return_type::ERROR;
